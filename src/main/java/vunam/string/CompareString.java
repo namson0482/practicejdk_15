0811a233ef0369dd3086899809836dfbc5ed1e55
Son Vu <son.vu@smartbear.com>
2022-08-13 17:26:04 +0700

    write object to file, compare string

---
+++ src/main/java/vunam/string/CompareString.java
@@ -0,0 +1,17 @@
+package vunam.string;
+
+public class CompareString {
+    public static void main(String[] args) {
+
+        String str1 = "abc";
+        String str2 = "abc";
+
+        String str3 = new String("abc");
+
+        System.out.println(str1 == str2);
+        System.out.println(str1 == str3);
+        System.out.println(str1.equalsIgnoreCase(str3));
+
+
+    }
+}
