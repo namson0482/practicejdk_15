576d0fb5a7199d6175b65c880e5c224f8a742fa6
Son Vu <namson0482@gmail.com>
2022-11-25 07:56:06 +0700

    refactor package

---
+++ src/main/java/vu/son/basic/InterestingTime.java
@@ -0,0 +1,84 @@
+package vu.son.basic;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InterestingTime {
+
+	public static long compareTime(String startTime, String endTime)  {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		long elapsed = 0;
+		try {
+			Date d1 = sdf.parse(startTime);
+			Date d2 = sdf.parse(endTime);
+			elapsed = d2.getTime() - d1.getTime();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return elapsed;
+	}
+	
+	public static boolean checkInterestingPoint(String time) {
+		
+		Map<String, String> mapValues = new HashMap<String, String>();
+		
+		for(int i=0;i<time.length();i++) {
+			if(!time.substring(i, i+1).equalsIgnoreCase(":") ) {
+				if(!mapValues.containsKey(time.substring(i, i+1))) {
+					mapValues.put(time.substring(i, i+1), time.substring(i, i+1));
+				}
+			}
+		}
+		
+		if(mapValues.size() <= 2) return true;
+		return false;
+		
+	}
+
+	public static int findInterestingPoint(String s, String t) {
+
+		String startTime = s;
+		int interestingPoint = 0;
+		while(compareTime(startTime, t)>=0) {
+			String[] endTimeArray = startTime.split(":");
+			if(checkInterestingPoint(startTime)) {
+				interestingPoint++;
+			}
+			int hours = Integer.parseInt(endTimeArray[0]);
+			int minutes = Integer.parseInt(endTimeArray[1]);
+			int seconds = Integer.parseInt(endTimeArray[2]);
+			
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	        Date date = new Date();
+	        Calendar c = Calendar.getInstance();
+	        c.setTime(date);
+	        
+	        c.set(Calendar.HOUR_OF_DAY, hours);
+	        c.set(Calendar.MINUTE, minutes);
+	        c.set(Calendar.SECOND, seconds + 1);
+	        Date currentDatePlusOne = c.getTime();
+	        String temp = dateFormat.format(currentDatePlusOne);
+	        startTime = temp.split(" ")[1];
+	        
+		}
+		
+		return interestingPoint;
+	}
+	
+	
+	public static void main(String[] args) {
+
+		String s = "22:22:21";
+		String t = "22:22:25";
+		System.out.println(findInterestingPoint(s, t));
+	}
+	 
+
+}
---
+++ src/main/java/vu/son/string/WordBreak.java
@@ -0,0 +1,42 @@
+package vu.son.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordBreak {
+
+	public static void main(String[] args) {
+		List<String> dict = new ArrayList<>();
+		dict.add("cat");
+		dict.add("cats");
+		dict.add("and");
+		dict.add("sand");
+		dict.add("dog");
+		System.out.println(new WordBreak().wordbreak("catsanddog", dict));
+	}
+
+	public boolean wordbreak(String words, List<String> dict) {
+		boolean mat[][] = new boolean[words.length()][words.length()];
+
+		// initialize matrix
+		for (int i = 0; i < words.length(); i++)
+			mat[i][i] = dict.contains(words.charAt(i) + "");
+
+		for (int l = 2; l <= words.length(); l++) {
+			for (int i = 0; i < words.length() - l + 1; i++) {
+				int j = i + l - 1;
+				if (dict.contains(words.substring(i, j + 1))) {
+					mat[i][j] = true;
+					continue;
+				}
+				for (int k = i; k < j; k++) {
+					if (mat[i][k] == true && mat[k + 1][j] == true) {
+						mat[i][j] = true;
+						continue;
+					}
+				}
+			}
+		}
+		return mat[0][words.length() - 1];
+	}
+}
