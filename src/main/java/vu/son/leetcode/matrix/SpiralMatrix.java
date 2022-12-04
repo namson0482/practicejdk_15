d38d165677899e5fc5cb35ba492180b16189e07b
Son Vu <namson0482@gmail.com>
2022-12-04 12:19:52 +0700

    Spiral matrix

--- src/main/java/vu/son/leetcode/matrix/SpiralMatrix.java
+++ src/main/java/vu/son/leetcode/matrix/SpiralMatrix.java
@@ -8,48 +8,50 @@ public class SpiralMatrix {
 
     public List<Integer> spiralOrderOne(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
-        int top = 0;
-        int left = 0;
-        int bottom = matrix.length - 1;
-        int right = matrix[0].length - 1;
         int rows = matrix.length;
         int columns = matrix[0].length;
-        int col, row;
-        while (result.size() < rows * columns) {
-            // left -> right
-            for (col = left; col <= right; col++) {
-                result.add(matrix[top][col]);
-            }
+        int up = 0;
+        int left = 0;
+        int right = columns - 1;
+        int down = rows - 1;
 
-            // move downward
-            for (row = top + 1; row < bottom; row++) {
+        while (result.size() < rows * columns) {
+            // Traverse from left to right.
+            for (int col = left; col <= right; col++) {
+                result.add(matrix[up][col]);
+            }
+            // Traverse downwards.
+            for (int row = up + 1; row <= down; row++) {
                 result.add(matrix[row][right]);
             }
-
-            // right -> left and make sure on a difference row
-            if(top != bottom) {
-                for (col = right - 1; col >= left; col--) {
-                    result.add(matrix[bottom][col]);
+            // Make sure we are now on a different row.
+            if (up != down) {
+                // Traverse from right to left.
+                for (int col = right - 1; col >= left; col--) {
+                    result.add(matrix[down][col]);
                 }
             }
-
             // Make sure we are now on a different column.
             if (left != right) {
                 // Traverse upwards.
-                for (row = bottom - 1; row > top; row--) {
+                for (int row = down - 1; row > up; row--) {
                     result.add(matrix[row][left]);
                 }
             }
             left++;
             right--;
-            top++;
-            bottom--;
+            up++;
+            down--;
         }
+
         return result;
     }
 
     public List<Integer> spiralOrder(int[][] matrix) {
-        int VISITED = 101;
+
+        // 0 not visited, 1 visited
+        int [][]visited = new int[matrix.length][matrix[0].length];
+        int VISITED = 1;
         int rows = matrix.length;
         int columns = matrix[0].length;
         // Four directions that we will move: right, down, left, up.
@@ -65,13 +67,15 @@ public class SpiralMatrix {
         // Store the first element and mark it as visited.
         List<Integer> result = new ArrayList<>();
         result.add(matrix[0][0]);
-        matrix[0][0] = VISITED;
+        visited[0][0] = VISITED;
         while (changeDirection < 2) {
+
+
             while (row + directions[currentDirection][0] >= 0 &&
                     row + directions[currentDirection][0] < rows &&
                     col + directions[currentDirection][1] >= 0 &&
                     col + directions[currentDirection][1] < columns &&
-                    matrix[row + directions[currentDirection][0]]
+                    visited[row + directions[currentDirection][0]]
                             [col + directions[currentDirection][1]] != VISITED) {
                 // Reset this to 0 since we did not break and change the direction.
                 changeDirection = 0;
@@ -79,7 +83,7 @@ public class SpiralMatrix {
                 row = row + directions[currentDirection][0];
                 col = col + directions[currentDirection][1];
                 result.add(matrix[row][col]);
-                matrix[row][col] = VISITED;
+                visited[row][col] = VISITED;
             }
             // Change our direction.
             currentDirection = (currentDirection + 1) % 4;
@@ -91,9 +95,13 @@ public class SpiralMatrix {
 
     public static void main(String[] args) {
 //        int [][] matrix = new int[][] {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}};
+
         int [][] matrix = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         SpiralMatrix spiralMatrix = new SpiralMatrix();
-        List list = spiralMatrix.spiralOrder(matrix);
+        List list = spiralMatrix.spiralOrderOne(matrix);
         list.forEach((name) -> System.out.print(name + " -> "));
+
+
+
     }
 }
