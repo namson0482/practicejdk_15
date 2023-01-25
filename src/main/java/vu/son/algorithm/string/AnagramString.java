c7488ed63bf512ec14403ae9b093c968aaa14529
Son Vu <namson0482@gmail.com>
2023-01-25 09:31:08 +0700

    add new files

---
+++ src/main/java/vu/son/algorithm/string/AnagramString.java
@@ -0,0 +1,21 @@
+package vu.son.algorithm.string;
+
+import java.util.Arrays;
+
+public class AnagramString {
+
+    boolean isAnagramSort(String string1, String string2) {
+        if (string1.length() != string2.length()) {
+            return false;
+        }
+        char[] a1 = string1.toCharArray();
+        char[] a2 = string2.toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        return Arrays.equals(a1, a2);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
