980c2885bbaa9db74f4ee84deceed334c4fc3900
Son Vu <namson0482@gmail.com>
2023-04-03 13:27:36 +0700

    13:27

--- src/main/java/com/vuson/algorithm/codility/Hospital.java
+++ src/main/java/com/vuson/algorithm/codility/Hospital.java
@@ -7,20 +7,21 @@ import java.util.Set;
 
 
 /**
- * There are N hospitals. numbered from 0 to N-1. You are given a schedule of work in each of the hospitals for the tollowing M davs. The schedule is provided in the form of a two.
- * dimensional array A containing N rows, each row representing the schedule of one nosota. anc v columns. each column reor esentino one cav. Intecer annuentorr within
- * the range O..N-1_ and L within the range ..M-1] represents the ID of the doctor working at hospital K on day L. Note that sometimes an individual doctor may work at more than
- * one hospital even on the same  days.
+ * There are N hospitals. numbered from 0 to N-1. You are given a schedule of work in each of the hospitals for the following M days.
+ * The schedule is provided in the form of a two-dimensional array A containing N rows, each row representing the schedule of one hospital.
+ * and M columns. each column representing one day. Integer A[K][L]
+ * K within the range [O..N-1] and L within the range [0..M-1] represents the ID of the doctor working at hospital K on day L.
+ * Note that sometimes an individual doctor may work at more than one hospital even on the same  days.
  *
  * Write a function:
  *  class Solution { public int solution(int [][]A);  }
  *
  * that, given a matrix A consisting of N rows and M columns representing the hospitals schedules, finds the number of doctors working at more than one hospital
  *
- * 1. Given A = 1. 2. 21. 3. 1. 4]] the function should return 1
+ * 1. Given A = [[1, 2, 2], [3, 1, 4]] the function should return 1
  *
- * The doctor with ID 1 works at both hospitals. The doctor with ID 2 works onlv at hospital number while the doctors with IDs 3 and 4 work onlv at hospital number .
- * 2. GIVen A = |1, 1, 5, 2, 31, 4, 5, 6, 4, 3, 19, 4, 4, 1, 5 , the tunction should return 4
+ * The doctor with ID 1 works at both hospitals. The doctor with ID 2 works only at hospital number while the doctors with IDs 3 and 4 work onlv at hospital number .
+ * 2. GIVen A = [[1, 1, 5, 2, 3], [4, 5, 6, 4, 3], [9, 4, 4, 1, 5]] the function should return 4
  *
  *
  * The doctors with IDs 1. 3, 4 and 5 work at more than one hospital.
@@ -57,7 +58,6 @@ public class Hospital {
                 op++;
             }
         }
-
         // number of doctors working at more than one hospital!
         return op;
     }
